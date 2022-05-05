82b9d37b7b81dc330be3e595ddc7d445d92e4266
MahammedK <mahammedkassam@gmail.com>
2022-05-05 14:44:37 +0000

    MK: Services 2 and 3 complete, 1 started

--- service_1/app.py
+++ service_1/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, request, Response
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_internet():
-    return "Hello Internet!"
+def home():
+    letter_drawn = requests.get('http://service_2:5000/letters')
+    prize_drawn = requests.get('http://service_3:5000/prizes')
+    return Response(f"{letter_drawn.text} {prize_drawn.text}", mimetype="text/plain")
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
--- service_2/app.py
+++ service_2/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, Response
+import random
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def hello_internet():
-    return "Hello Internet!"
+raffle_letters = ["O", "L", "M", "K", "A", "U", "B", "N", "I", "D", "H", "V", "E"]
+
+@app.route('/letters', methods=['GET'])
+def letters():
+    letter_selected = random.choice(raffle_letters)
+    return Response(f"{letter_selected}", mimetype='text/plain')
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
