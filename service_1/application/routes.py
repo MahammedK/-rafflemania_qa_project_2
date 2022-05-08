6a051e25dd04b8b1ab418ff60fdf5ac95eeacd45
MahammedK <mahammedkassam@gmail.com>
2022-05-08 12:54:16 +0000

    MK: application running, service 4 needs working

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -1,16 +1,17 @@
 from application import app 
-from flask import Flask, request, Response, jsonify
+from flask import Flask, request, Response, jsonify, render_template
 import requests
 
 @app.route('/')
 def home():
-    letter_drawn = requests.get('http://service_2:5000/letters').json()
-    prize_drawn = requests.get('http://service_3:5000/prizes').json()
+    letter_drawn = requests.get('http://service_2:5000/letters')
+    prize_drawn = requests.get('http://service_3:5000/prizes')
 
-    fields = {'letter_drawn': letter_drawn, 'prize_drawn': prize_drawn}
+    # fields = {'letter_drawn': letter_drawn, 'prize_drawn': prize_drawn}
 
-    final_draw = requests.get('http://service_4:5000/secondround', json=fields)
+    final_draw = requests.get('http://service_4:5000/secondround')
 
-    return Response(f"{letter_drawn.text} {prize_drawn.text} {final_draw}", mimetype="text/plain") #change response for html and remove mimetype
+    raffle_prize = f"Raffle letter '{letter_drawn.text}', gets the prize of '{prize_drawn.text}' and also '{final_draw.text}'" 
+    return render_template('index.html', raffle_prize=raffle_prize)
 
     # return render_template('main.html', final_draw=final_draw.json())
--- service_4/application/routes.py
+++ service_4/application/routes.py
@@ -1,12 +1,13 @@
 from application import app
 from flask import Flask, request, jsonify, Response
+import requests
 
 extra_prize = {0: "AirPods", 1: "QA CloudAcademy free demo"}
 vowels = "AEIOUaeiou"
 
 @app.route('/secondround', methods=['POST'])
 def secondround():
-    get_letter = (request.get_json()['service_2'])
+    get_letter = requests.get('service_2')
 
     for vowels in get_letter:
         if vowels in get_letter:
